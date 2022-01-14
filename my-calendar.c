1e3600016af59b65b613fdb27802066de6a0b2cb
Md. Zaif Imam Mahi <mdzaifimammahi@gmail.com>
2022-01-14 11:16:28 +0600

    Update my-calendar.c

--- my-calendar.c
+++ my-calendar.c
@@ -1,47 +1,64 @@
 #include<stdio.h>
+//first declare the color function.
 void red(){
     printf("\033[1;31m");
 }
 void green(){
-    printf("\033[0;32m");
+    printf("\033[0;32m");//
 }
 void cyan(){
     printf("\033[0;36m");
 }
-
+//now create a function to stop the color function.
 void reset () {
   printf("\033[0m");
 }
+//declare the mathematical function to find out the first day of a month.
 int getfirstdayoftheyear(int y){
     int day = ((y * 365 + (y - 1) / 4) - ((y - 1) / 100) + ((y -1) / 400)) % 7;
     return day;
 }
+//enter the main function.
 int main()
 {
+    //declare the character array or stirng to assign the name for months.
     char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    //declare the ranges of each month in an array.
     int daysmonths[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    //
     int i, j, totaldays, weekdays = 0, spacecounter = 0, year;
+    //print a message to user to input a month/
     printf("Enter a year:\n");
-    scanf("%d", &year);
+    scanf("%d", &year);// user input function.
+    //input the color in welcome message.
     green();
     printf("\n\n***************** Welcome to %d *******************\n\n", year);
+    //stop the color function.
     reset();
 
+    // give a logic for leap year.
     if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
         daysmonths[1] = 29;
     }
+    // call getfirstdayoftheyear() and assign the day in weekdays.
     weekdays = getfirstdayoftheyear(year);
 
+    //generate a loop for orientation of the calender and print the calendar.
     for(i = 0; i < 12; i++){
+        //input the color in months messages.
         cyan();
         printf("\n\n\n--------------------------- %s --------------------------\n", months[i]);
+        //stop the color function.
         reset();
 
+        //print the message of day's name.
         printf("\n   Sun   Mon   Tue   Wed   Thu");
+        //assign the color in fri and sat.
         red();
         printf("   Fri   Sat\n\n");
         reset();
 
+        //Orientation section.
         for(spacecounter = 1; spacecounter <= weekdays; spacecounter++){
                  printf("      ");
 
